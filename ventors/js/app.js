37d78d01322d78ae6c3bd6da9ff0db626c0e9220
Yoss <yoseline_96@hotmail.com>
2017-12-22 06:37:44 -0500

    Mejorando interacción con jQuery

--- ventors/js/app.js
+++ ventors/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 var imgItems = $('.slider li').length; //Número de slides
-
+var imgPos = 1;
 //Agregando pagination
 for(var i= 0; i<imgItems; i++){
   $('.pagination').append('<li><span class="fa fa-circle"></span></li>');
@@ -10,59 +10,47 @@ for(var i= 0; i<imgItems; i++){
   $('.pagination li span:first').css({'color':'rgb(126,214,224)'}); //Damos estilos al primer span de pagination
 
 //Ejecutamos las Funciones
-
+//Función para los botones
 $('.pagination li').click(function(){
   var paginationPos = $(this).index() + 1;
   $('.slider li').hide()
   $('.slider li:nth-child('+ paginationPos +')').fadeIn(); //Mostramos el slide seleccionado
 
   //Dando estilos
-/*  $('.pagination li span').css({'color':'#FFF'});*/
+  $('.pagination li span').css({'color':'#FFF'});
   $(this).css({'color':'rgb(126,214,224)'});
+
+  imgPos = paginationPos;
+
 });
 
+//Función para la flecha de la derecha
 $('.fa-chevron-right').click(function(){
+  if(imgPos >= imgItems){
+    imgPos = 1;
+  } else{
+    imgPos ++;
+  }
 
+  $('.pagination li span').css({'color':'#FFF'});
+  $('.pagination li:nth-child('+ imgPos +')').css({'color':'rgb(126,214,224)'});
+
+  $('.slider li').hide()
+  $('.slider li:nth-child('+ imgPos +')').fadeIn();
+});
+
+//Función para la flecha de la izquierda.
+$('.fa-chevron-left').click(function(){
+  if(imgPos <= 1){
+    imgPos = imgItems;
+  } else{
+    imgPos --;
+  }
+
+  $('.pagination li span').css({'color':'#FFF'});
+  $('.pagination li:nth-child('+ imgPos +')').css({'color':'rgb(126,214,224)'});
+
+  $('.slider li').hide()
+  $('.slider li:nth-child('+ imgPos +')').fadeIn();
 });
 });
-
-/*
-var loadPage = function(){
-  //Elementos
-  var $buttons = $('.control');
-  var $previous = $('.previous');
-  var $next = $('.next');
-
-  //Eventos
-  $buttons.click(changeImage);
-  $previous.click(previousImage);
-  $next.click(nextImage);
-};
-
-//Funciones
-
-var changeImage = function(){
-  target = parseInt($(this).data('target'));
-  showImage(target);
-};
-
-var previousImage = function(e){
-  e.preventDefault();
-  target = target - 1;
-  target = (target < 0) ? 4 : target;
-  showImage(target);
-};
-
-var nextImage = function(e){
-  e.preventDefault();
-  target = target + 1;
-  target = (target > 4) ? 0 : target;
-  showImage(target);
-};
-
-var showImage = function(target){
-  var $lastSlide = $('div.active');
-  var $slide = $("div[data-slide='" + target + "']");
-  $lastSlide.removeClass =('active');
-  $slide.addClass =('active');
-};*/
